779777c532734d238a52952d651e37ef0b02c658
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-08-15 16:40:34 -0300

    fix locastorage functions

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,15 +1,22 @@
+const changeEvent = new Event('LocalStorageChanged');
+
 const LocalStorage = {
-  get: () => JSON.parse(localStorage.getItem('data')),
+  get() {
+    return JSON.parse(localStorage.getItem('data'));
+  },
 
-  updateWith: data => localStorage.setItem('data', JSON.stringify(data)),
+  updateWith(data) {
+    localStorage.setItem('data', JSON.stringify(data));
+    document.dispatchEvent(changeEvent);
+  },
 
-  add: newItem => {
+  add(newItem) {
     const data = this.get();
     data.push(newItem);
     this.updateWith(data);
   },
 
-  deleteAt: pos => {
+  deleteAt(pos) {
     const data = this.get();
     const newData = data.filter((_, i) => i !== pos);
     this.updateWith(newData);
