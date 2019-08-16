597c3245be8f84714451882df783e5ab1911ec1e
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-08-16 11:38:01 -0300

    format local storage

--- tests/localStorage.test.js
+++ tests/localStorage.test.js
@@ -1,62 +1,61 @@
 import LocalStorage from '../src/js/localStorage';
 
 const mock = {
-  name: 'Test',
-  email: 'test@test.com',
-  phone: '1122233344',
-  cpf: '050050050',
+    name: 'Test',
+    email: 'test@test.com',
+    phone: '1122233344',
+    cpf: '050050050'
 };
 
-LocalStorage.add = jest.fn((item) => {
-  const data = [mock];
-  data.push(item);
-  return data;
+LocalStorage.add = jest.fn(item => {
+    const data = [mock];
+    data.push(item);
+    return data;
 });
 
-LocalStorage.deleteAt = jest.fn((position) => {
-  const data = [mock].filter((_, j) => position !== j);
-  return data;
+LocalStorage.deleteAt = jest.fn(position => {
+    const data = [mock].filter((_, j) => position !== j);
+    return data;
 });
-
 
 describe('Local Storage', () => {
-  beforeEach(() => {
-    LocalStorage.updateWith(mock);
-  });
+    beforeEach(() => {
+        LocalStorage.updateWith(mock);
+    });
 
-  afterEach(() => {
-    LocalStorage.clean();
-  });
+    afterEach(() => {
+        LocalStorage.clean();
+    });
 
-  it('should return all values with get', () => {
-    expect(LocalStorage.get()).toEqual(mock);
-  });
+    it('should return all values with get', () => {
+        expect(LocalStorage.get()).toEqual(mock);
+    });
 
-  it('should add a new item with add', () => {
-    const newItem = {
-      name: 'Test 2',
-      email: 'test2@email.com',
-      phone: '9393939393',
-      cpf: '030339393',
-    };
+    it('should add a new item with add', () => {
+        const newItem = {
+            name: 'Test 2',
+            email: 'test2@email.com',
+            phone: '9393939393',
+            cpf: '030339393'
+        };
 
-    expect(LocalStorage.add(newItem)).toEqual([mock, newItem]);
-  });
+        expect(LocalStorage.add(newItem)).toEqual([mock, newItem]);
+    });
 
-  it('should replace the data with updateWith', () => {
-    const newData = {
-      name: 'test 3',
-      email: 'test3@email.com',
-      phone: '92838383',
-      cpf: '2398238293',
-    };
+    it('should replace the data with updateWith', () => {
+        const newData = {
+            name: 'test 3',
+            email: 'test3@email.com',
+            phone: '92838383',
+            cpf: '2398238293'
+        };
 
-    LocalStorage.updateWith(newData);
+        LocalStorage.updateWith(newData);
 
-    expect(LocalStorage.get()).toEqual(newData);
-  });
+        expect(LocalStorage.get()).toEqual(newData);
+    });
 
-  it('should delete one item with delete', () => {
-    expect(LocalStorage.deleteAt(0)).toEqual([]);
-  });
+    it('should delete one item with delete', () => {
+        expect(LocalStorage.deleteAt(0)).toEqual([]);
+    });
 });
